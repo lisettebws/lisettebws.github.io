1e03ea920ed3357c2cbd8b3a38a947da4458aca1
lisettebws <142450959+lisettebws@users.noreply.github.com>
2024-02-16 13:33:35 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import '/material_web/button/filled-button.js';
-import '/material_web/button/outlined-button.js';
-import '/node_modules/@material_web/checkbox/checkbox.js';
+import '@material/web/button/filled-button.js';
+import '@material/web/button/outlined-button.js';
+import '@material/web/checkbox/checkbox.js';
